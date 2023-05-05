932e5bfc869ed0ff4045991972dcf396657d7d99
M. Canul <canulmauricio03@gmail.com>
2023-05-05 15:50:30 -0400

    got it to work within TypeScript

--- scripts/initial.ts
+++ scripts/initial.ts
@@ -1,1 +1,17 @@
-console.log("henlo");
+var firebase;
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBeJWnW2QZnH2DV0iB4ee23dUji9szWuUw",
+    authDomain: "iddlehammer.firebaseapp.com",
+    databaseURL: "https://iddlehammer-default-rtdb.firebaseio.com",
+    projectId: "iddlehammer",
+    storageBucket: "iddlehammer.appspot.com",
+    messagingSenderId: "1099237781233",
+    appId: "1:1099237781233:web:7633157ce73673fe694350"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+(function(){
+    console.log("henlo");
+    firebase.auth().signInAnonymously();
+})();
